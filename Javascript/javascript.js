1fd23dd3c8b8b89b0185fe7bbc8a6db7633777f6
Naz758 <72582856+Naz758@users.noreply.github.com>
2020-12-04 18:04:26 -0400

    Add files via upload

---
+++ Javascript/javascript.js
@@ -0,0 +1,13 @@
+
+
+function testCode()
+{
+    var sizeprice = document.getElementById("size").value;
+    var crustprice = document.getElementById("crust").value;
+    var test = sizeprice + 1;
+}
+
+function test()
+{
+    document.getElementById("testbox").innerHTML = "it works";
+}
